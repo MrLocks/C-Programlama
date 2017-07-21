585b8d9c70baa851e291bb9afb36f7ba33bccd04
MrLocks <fkmll2121@gmail.com>
2017-07-22 02:03:06 +0300

    aklındaki sayiyi bulma

---
+++ aklindaki sayiyi bulma.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+	
+	
+	int a;
+	
+	printf("aklindan bir sayi tutun ve sayiyi girin;"); scanf("%d",&a);
+	
+	printf("sayiyi 3 ile carpiniz ve entere basiniz:");
+	getchar();
+	printf("buldugunuz sonuca 21 ekleyiniz ve entere basiniz:");
+	getchar();
+	printf("bu sonucu 3 e bolunuz ve entere basiniz:");
+	getchar();
+	printf("bu sonuctan basta tutugunuz sayiyi cikariniz ve entere basiniz;");
+	
+	getchar();
+	
+	printf("sonuc 7");
+	 
+	
+	getchar();
+}
